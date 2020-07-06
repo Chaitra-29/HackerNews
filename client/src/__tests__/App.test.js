28318bfe1e2b16bbd1bbe612516eb828ba5fbf77
Bhagavan, Chaitra <>
2020-07-06 20:42:41 +0530

    formatting

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import App from '../App';
-import { render,screen } from "@testing-library/react";
-import { configure,shallow } from 'enzyme';
+import { render, screen } from "@testing-library/react";
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 test('renders column headers', () => {
-  render(<App match={{ params: { id: 0 } }}  />);
+  render(<App match={{ params: { id: 0 } }} />);
   const comments = screen.getByText(/Comments/i);
   const voteCounts = screen.getByText(/Vote Counts/i);
   const upVote = screen.getByText(/UpVote/i);
@@ -18,15 +18,15 @@ test('renders column headers', () => {
   expect(newsDetails).toBeInTheDocument();
 });
 
-test('componentDidMount and callAPI are called' , () => {
+test('componentDidMount and callAPI are called', () => {
   const componentDidMount = jest.spyOn(App.prototype, 'componentDidMount');
   const callAPI = jest.spyOn(App.prototype, 'callAPI');
-  render(<App match={{ params: { id: 0 } }}  />);
+  render(<App match={{ params: { id: 0 } }} />);
   expect(componentDidMount).toHaveBeenCalledTimes(1);
   expect(callAPI).toHaveBeenCalledTimes(1);
 });
 const response = {
-  hits:[{
+  hits: [{
     "created_at": "2020-07-05T10:44:30.000Z",
     "title": null,
     "url": null,
@@ -41,50 +41,50 @@ const response = {
     "parent_id": 23737460,
     "created_at_i": 1593945870,
     "_tags": [
-        "comment",
-        "author_gas9S9zw3P9c",
-        "story_23737460"
+      "comment",
+      "author_gas9S9zw3P9c",
+      "story_23737460"
     ],
     "objectID": "23737785",
     "_highlightResult": {
-        "author": {
-            "value": "gas9S9zw3P9c",
-            "matchLevel": "none",
-            "matchedWords": []
-        },
-        "comment_text": {
-            "value": "Not myself, but I know someone who was making a living from betting on horse racing. It's not that different from &quot;gambling&quot; in the stock market. The same principles apply. You need some kind of egde, which often is 1. Data or 2. Market Access, less often the statistical model. Better data beats better models, and modeling is commoditized. Also, you need a pretty decent bankroll so that you can handle the volatility.",
-            "matchLevel": "none",
-            "matchedWords": []
-        },
-        "story_title": {
-            "value": "Ask HN: Who Earns a Living of Betting?",
-            "matchLevel": "none",
-            "matchedWords": []
-        },
-        "nbHits": 21999276,
-        "page": 0,
-        "nbPages": 34,
-        "hitsPerPage": 30,
+      "author": {
+        "value": "gas9S9zw3P9c",
+        "matchLevel": "none",
+        "matchedWords": []
+      },
+      "comment_text": {
+        "value": "Not myself, but I know someone who was making a living from betting on horse racing. It's not that different from &quot;gambling&quot; in the stock market. The same principles apply. You need some kind of egde, which often is 1. Data or 2. Market Access, less often the statistical model. Better data beats better models, and modeling is commoditized. Also, you need a pretty decent bankroll so that you can handle the volatility.",
+        "matchLevel": "none",
+        "matchedWords": []
+      },
+      "story_title": {
+        "value": "Ask HN: Who Earns a Living of Betting?",
+        "matchLevel": "none",
+        "matchedWords": []
+      },
+      "nbHits": 21999276,
+      "page": 0,
+      "nbPages": 34,
+      "hitsPerPage": 30,
     }
-},]
+  },]
 }
-test('fetch api' , () => {
-  const upvotes = jest.spyOn(App.prototype, 'getLocalStorage').mockImplementation(() => []); 
+test('fetch api', () => {
+  const upvotes = jest.spyOn(App.prototype, 'getLocalStorage').mockImplementation(() => []);
   const mockJsonPromise = Promise.resolve(response);
   const mockFetchPromise = Promise.resolve({
     json: () => mockJsonPromise,
   });
-  jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise); 
-  const wrapper = shallow(<App match={{ params: { id: 0 } }} />); 
+  jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+  const wrapper = shallow(<App match={{ params: { id: 0 } }} />);
   expect(global.fetch).toHaveBeenCalledTimes(1);
   expect(global.fetch).toHaveBeenCalledWith('http://hn.algolia.com/api/v1/search_by_date?tags=front_page&hitsPerPage=10&page=0');
 });
 
-test('previous and next button' , () => {
+test('previous and next button', () => {
   const goBack = jest.fn();
   const push = jest.fn();
-  const wrapper = shallow(<App match={{ params: { id: 0 } }} history={{goBack: goBack, push:push}} />);
+  const wrapper = shallow(<App match={{ params: { id: 0 } }} history={{ goBack: goBack, push: push }} />);
   expect(wrapper.find('button').at(0).simulate('click'));
   expect(goBack).toHaveBeenCalledTimes(1);
   expect(wrapper.find('button').at(1).simulate('click'));
