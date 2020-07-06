31fc7bdb88f70015534952be718de110bdf3b8c5
Bhagavan, Chaitra <>
2020-07-06 22:30:40 +0530

    removing used constructor

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Welcome extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     navigate() {
         this.props.history.push('0')
     }
