2447f0f1a76ce165b25d7c671c318c5d8814f7fe
Bhagavan, Chaitra <>
2020-07-07 22:29:51 +0530

    updated button

--- src/Welcome.js
+++ src/Welcome.js
@@ -10,7 +10,7 @@ class Welcome extends Component {
             <div className='jumbotron jumbotron-no-margin'>
                 <div className='container'>
                     <h1 className='display-4'>HackerNews!</h1>
-                    <button className="btn dark-orange-color btn-lg" onClick={this.navigate.bind(this)}>News</button>
+                    <button className="btn dark-orange-color btn-lg" onClick={this.navigate.bind(this)}>Click</button>
                 </div>
             </div>
         );
